65836a4d60d9b1ccaba615a989bfc9440f18aa03
hassohamid <hassohamid@hotmail.com>
2025-04-22 15:08:57 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import SplineNameTag from "./components/SplineNameTag";
+
+function App() {
+  return (
+    <div className="App">
+      <SplineNameTag />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SplineNameTag.jsx
@@ -0,0 +1,7 @@
+import Spline from "@splinetool/react-spline";
+
+export default function SplineNameTag() {
+  return (
+    <Spline scene="https://prod.spline.design/BS6zghKhnCv2sXoz/scene.splinecode" />
+  );
+}
